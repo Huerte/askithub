5050617a2c79e5bc4df27ed3eb9177c4de838002
Huerte <huertejerald@gmail.com>
2025-07-19 11:42:21 +0800

    Added a login page and improve the homepage

--- src/forum/views.py
+++ src/forum/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def homepage_view(requests):
     return render(requests, 'forum/home.html')
+
+def login_view(requests):
+    return render(requests, 'forum/auth/login.html')
