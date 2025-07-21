cb2e4706240a92076365c303523bb11c6c3c91c0
Huerte <huertejerald@gmail.com>
2025-07-21 14:34:00 +0800

    Delete forum directory

--- forum/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.homepage_view, name='homepage'),
-    path('login/', views.login_view, name='login-page'),
-    path('register/', views.register_view, name='register-page'),
-]
