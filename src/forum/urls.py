79f03d88a943542f4f6451f365ce506afb258266
Huerte <huertejerald@gmail.com>
2025-07-28 14:17:04 +0800

    Added a topic page and a dynamic topic sidebar at homepage

--- src/forum/urls.py
+++ src/forum/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('remove-answer/<str:comment_id>', views.remove_comment, name='remove_answer'),
     path('add-question/', views.add_question, name='add_question'),
     path('delete-question/<str:room_id>', views.delete_question, name='delete_question'),
+
+    path('related-questions/<str:topic_id>', views.visit_topics, name='visit_related_topics'),
 ]
