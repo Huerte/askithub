6b09bed4d2b3b2c6e645a3db8e1a9137e0a5e772
Huerte <huertejerald@gmail.com>
2025-08-03 11:40:28 +0800

    Added an edit comment feature

--- src/forum/urls.py
+++ src/forum/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('answer/<str:room_id>', views.comment, name='answer_question'),
     path('remove-answer/<str:comment_id>', views.remove_comment, name='remove_answer'),
     path('edit-answer/<str:answer_id>', views.edit_answer, name='edit_answer'),
+    path('answer/<int:answer_id>/get/', views.get_answer_body, name='get_answer_body'),
 
     path('add-question/', views.add_question, name='add_question'),
     path('delete-question/<str:room_id>', views.delete_question, name='delete_question'),
