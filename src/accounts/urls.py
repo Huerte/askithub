c20106e72bbb01acd1d4e5929af8fdeb4fa7baf0
Huerte <huertejerald@gmail.com>
2025-08-14 07:43:27 +0800

    Add a settings page

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('followers/<str:user_id>', views.view_followers, name='visit_followers'),
     path('following/<str:user_id>', views.view_following, name='visit_following'),
 
+    path('settings/', views.enter_settings, name='enter_settings'),
+
 ]
